f5a65f73c874a1e7cbec9cc1c1a7f4b961f14650
Rich Chang <rich_chang@outlook.com>
2019-11-16 23:00:34 +0800

    Add answer and generateQuestion method

--- app/src/main/java/com/richc/braintrainer/MainActivity.java
+++ app/src/main/java/com/richc/braintrainer/MainActivity.java
@@ -3,12 +3,26 @@ package com.richc.braintrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
+    TextView sumTextView;
+    TextView resultTextView;
+    Button ulButton;
+    Button urButton;
+    Button dlButton;
+    Button drButton;
+    ArrayList<Integer> answers = new ArrayList<>();
+    int correctAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +30,68 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         startButton = findViewById(R.id.startButton);
-        startButton.setVisibility(View.VISIBLE);
+        startButton.setVisibility(View.INVISIBLE);
+
+        sumTextView = findViewById(R.id.sumTextView);
+        resultTextView = findViewById(R.id.resultTextView);
+        ulButton = findViewById(R.id.upLeftButton);
+        urButton = findViewById(R.id.upRightButton);
+        dlButton = findViewById(R.id.downLeftButton);
+        drButton = findViewById(R.id.downRightButton);
+
+        resultTextView.setText("");
+
+        generateQuestion();
+        
     }
 
 
-    // goBtn click
+    // startBtn click
     public void start(View view) {
         startButton.setVisibility(View.INVISIBLE);
+    }
+
+    // answerBtn click
+    public void Answer(View view) {
+        Log.i("tag", view.getTag().toString());
+
+        // Check if correct
+        if (answers.get(Integer.valueOf(view.getTag().toString())) == correctAnswer) {
+            Log.i("answer", "Correct !!!");
+
+            resultTextView.setText("Correct !");
+
+            generateQuestion();
+
+        } else {
+            resultTextView.setText("Wrong !");
+        }
+    }
+
+    public void generateQuestion() {
+
+        answers.clear();
+
+        Random rand = new Random();
+        int a = rand.nextInt(14);
+        int b = rand.nextInt(14);
+        correctAnswer = a+b;
+
+        sumTextView.setText(String.format("%d",a) + " + " + String.format("%d",b));
+
+        int correctAnsLocation = rand.nextInt(4);
+
+        for (int i=0; i<4; i++) {
+            if (i==correctAnsLocation) {
+                answers.add(correctAnswer);
+            } else {
+                answers.add(rand.nextInt(40));
+            }
+        }
+
+        ulButton.setText(Integer.toString(answers.get(0)));
+        urButton.setText(Integer.toString(answers.get(1)));
+        dlButton.setText(Integer.toString(answers.get(2)));
+        drButton.setText(Integer.toString(answers.get(3)));
     }
 }
